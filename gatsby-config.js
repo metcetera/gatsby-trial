60f58692f043d1e35bc48c59184f4e6cb88fea68
infonl-marcel <marcele@info.nl>
2022-10-28 16:08:45 +0200

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Trial Site",
+    title: "Gatsby Trial Site Whaeteaver",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
